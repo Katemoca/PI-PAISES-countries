10bd9dc8fb21420e25d5f968c87cca6a98e1a69d
Katerin <katerinmonguacastro@gmail.com>
2023-08-01 15:58:51 -0500

    changes done

--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -1,12 +1,16 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Country', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Country",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false, freezeTableName: true }
+  );
 };
